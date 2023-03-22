5ee5ed3e2893877866b0abe576d88ea0d1f37c41
karimou cisse <karimou@pylote.io>
2023-03-22 16:34:02 +0100

    [ADD] files: auth.router.js, list.router.js and todo.list.js [ADD] files: List.Schema.js, Todo.Schema.js, User.Schema.js

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
+require("dotenv").config();
 const express = require("express");
+const { connectToMongodb } = require("./config/database");
+const authRouter = require("./routes/auth.router");
+const listRouter = require("./routes/list.router");
+const todoRouter = require("./routes/todo.router");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+connectToMongodb();
+
+app.use(express.json());
+
+app.use("/auth", authRouter);
+app.use("/list", listRouter);
+app.use("/todo", todoRouter);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
