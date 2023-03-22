acdc2692e349dad3de77321077031428ad0455e1
karimou cisse <karimou@pylote.io>
2023-03-22 17:35:10 +0100

    [CHANGE] add POST/signup action in auth.router.js

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,8 +1,15 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const User = require("../modeles/User.Schema")
 
 router.post("/signup", async (req, res, next) => {
   try {
-    
+    const {password, email} = req.body
+    const saltedRounds = 10
+    const hashedPassword = await bcrypt.hash(password, saltedRounds)
+    const response = await User.create({email, password: hashedPassword})
+    res.status(201).json({message: "created user"})
   } catch (error) {
     next(error);
   }
