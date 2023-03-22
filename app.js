16fa50b080fb16532c02b2419bf7370a5501d131
karimou cisse <karimou@pylote.io>
2023-03-22 16:04:40 +0100

    [ADD] file settings.json

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const express = require("express")
-const app = express()
-const PORT = process.env.PORT || 5000
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
-})
+  console.log(`server is running on port ${PORT}`);
+});
